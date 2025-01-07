8eed71385f60c9e3873e30913ace1c774000d096
FursykIvan <ivan.fursyk@gmail.com>
2025-01-07 22:11:17 +0200

    fix permissions for user

--- api_service_theatre/settings.py
+++ api_service_theatre/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "drf_spectacular"
     "rest_framework",
+    "rest_framework_simplejwt"
     "theatre",
     "user",
 ]
@@ -128,6 +129,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = "static/"
+
+MEDIA_URL = "/media/"
+
+MEDIA_ROOT = BASE_DIR / "media"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
