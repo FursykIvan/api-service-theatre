b5e1cad7158e8ec8eae63151a1640bb0ef7a0575
FursykIvan <ivan.fursyk@gmail.com>
2025-01-16 13:30:51 +0200

    Add support for PostgreSQL in Docker environment and improve database readiness
    - Updated settings.py to configure PostgreSQL database in Docker environment and fallback to SQLite for local setup.
    - Added a custom management command wait_for_db to ensure database availability before starting the application.
    - Modified docker-compose.yml to include necessary environment variables for PostgreSQL configuration.
    - Updated equirements.txt to include dependencies for PostgreSQL support.
    - Added .dockerignore file to improve Docker build process.
    - Created initial migrations for the user app.

--- api_service_theatre/settings.py
+++ api_service_theatre/settings.py
@@ -84,16 +84,26 @@ WSGI_APPLICATION = "api_service_theatre.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.environ.get("POSTGRES_DB", "theatre"),
-        "USER": os.environ.get("POSTGRES_USER", "theatre"),
-        "PASSWORD": os.environ.get("POSTGRES_PASSWORD", "theatre"),
-        "HOST": os.environ.get("POSTGRES_HOST", "db"),
-        "PORT": os.environ.get("POSTGRES_PORT", "5433"),
+
+
+if os.environ.get("IN_DOCKER", False):
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.postgresql",
+            "NAME": os.environ.get("POSTGRES_DB", "theatre"),
+            "USER": os.environ.get("POSTGRES_USER", "theatre"),
+            "PASSWORD": os.environ.get("POSTGRES_PASSWORD", "theatre"),
+            "HOST": os.environ.get("POSTGRES_HOST", "db"),
+            "PORT": os.environ.get("POSTGRES_PORT", "5432"),
+        }
     }
-}
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
 
 AUTH_USER_MODEL = "user.User"
 
